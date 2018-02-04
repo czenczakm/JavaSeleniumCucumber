fe3687cc2ce8270558c8bf6c9f2adc5a9b1e8a86
czenczakm <czenczakm@gmail.com>
2018-02-04 23:10:44 +0100

    added utils and testng.xml

---
+++ src/test/java/utils/Log.java
@@ -0,0 +1,43 @@
+package utils;
+
+import org.apache.log4j.Logger;
+
+public class Log {
+    private static final Logger log = Logger.getLogger(Log.class.getName());
+
+    public static void startTestCase(String sTestCaseName) {
+        Log.info("****************************************************************************************");
+        Log.info("****************************************************************************************");
+        Log.info("$$$$$$$$$$$$$$$$$$$$$                 " + sTestCaseName + "       $$$$$$$$$$$$$$$$$$$$$$$$$");
+        Log.info("****************************************************************************************");
+        Log.info("****************************************************************************************");
+    }
+
+    public static void endTestCase(String sTestCaseName) {
+        Log.info("XXXXXXXXXXXXXXXXXXXXXXX             " + "-E---N---D-" + "             XXXXXXXXXXXXXXXXXXXXXX");
+        Log.info("X");
+        Log.info("X");
+        Log.info("X");
+        Log.info("X");
+    }
+
+    public static void info(String message) {
+        log.info(message);
+    }
+
+    public static void warn(String message) {
+        log.warn(message);
+    }
+
+    public static void error(String message) {
+        log.error(message);
+    }
+
+    public static void fatal(String message) {
+        log.fatal(message);
+    }
+
+    public static void debug(String message) {
+        log.debug(message);
+    }
+}
